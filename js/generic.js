a2fc165f6f9b6e671c91d46f0e3252c1e1a26992
h3rby7 <>
2022-02-04 18:46:50 +0100

    extract loop to js

--- js/generic.js
+++ js/generic.js
@@ -1,16 +1,12 @@
-class AudioOptions {
-  isSfx = true;
-}
-
 class AudioMeta {
   id;
-  isRunning = false;
+  isPlaying = false;
   button;
   audio;
   options;
-  constructor(id, options) {
+  constructor(id) {
     this.id = id;
-    this.options = options ? options : new AudioOptions();
+    this.init();
   }
 
   play() {
@@ -24,6 +20,10 @@ class AudioMeta {
     this.audio.currentTime = 0;
   }
 
+  getMsLeft() {
+    return 1000 * (this.audio.duration - this.audio.currentTime);
+  }
+
   init() {
     console.log("Binding '" + this.id + "'");
 
@@ -32,36 +32,95 @@ class AudioMeta {
     this.button = container.querySelector("button");
     this.audio = container.querySelector("audio");
 
-    // add CLASSES by options
-    if (this.options.isSfx) {
-      this.button.classList.add("reset");
-    }
-
     // STATE TRANSITIONS by Events
     this.audio.addEventListener("ended", () => {
-      this.isRunning = false;
+      this.isPlaying = false;
       this.button.classList.remove("playing");
     });
     this.audio.addEventListener("pause", () => {
-      this.isRunning = false;
+      this.isPlaying = false;
       this.button.classList.remove("playing");
     });
     this.audio.addEventListener("play", () => {
-      this.isRunning = true;
+      this.isPlaying = true;
       this.button.classList.add("playing");
     });
+  }
+}
 
-    // ACTIONS
+class StoppableAudio extends AudioMeta {
+  init() {
+    super.init();
     this.button.addEventListener("click", () => {
-      if (this.options.isSfx) {
-        this.audio.currentTime = 0;
-        return this.play();
-      }
-      if (!this.isRunning) {
+      if (!this.isPlaying) {
         return this.play();
       } else {
         return this.stop();
       }
     });
+  }
+}
+
+class SoundEffect extends AudioMeta {
+  init() {
+    super.init();
+    this.button.classList.add("reset");
+    this.button.addEventListener("click", () => {
+      this.audio.currentTime = 0;
+      return this.play();
+    });
+  }
+}
+
+// Latency between telling to use the 'loop-out' and the predicted start of the soundfile.
+// Higher Value means earlier Start of 'loop-out'
+const playLatencyMs = 250;
+
+class QuitableLoop {
+
+  loopAudioMeta;
+  endAudioMeta;
+
+  exitTimeout;
+
+  constructor(loopSelector, endSelector) {
+    this.loopAudioMeta = new AudioMeta(loopSelector);
+    this.endAudioMeta = new AudioMeta(endSelector);
+    this.init();
+  }
+
+  init() {
+    this.loopAudioMeta.button.addEventListener("click", () => {
+      this.startLoop();
+    });
+    this.endAudioMeta.button.addEventListener("click", () => {
+      this.exitLoopAndEnd();
+    });
+  }
+
+  startLoop() {
+    this.endAudioMeta.stop();
+    this.loopAudioMeta.audio.loop = true;
+    this.loopAudioMeta.play();
+  }
+
+  stop() {
+    this.endAudioMeta.stop();
+    this.loopAudioMeta.stop();
+  }
+
+  exitLoopAndEnd() {
+    this.loopAudioMeta.audio.loop = false;
+    this.endAudioMeta.audio.currentTime = 0;
+    if (this.exitTimeout) {
+      clearTimeout(this.exitTimeout);
+    }
+    const startIn = this.loopAudioMeta.getMsLeft() - playLatencyMs;
+    this.exitTimeout = setTimeout(() => {
+      this.endAudioMeta.audio.currentTime = 0;
+      this.endAudioMeta.play();
+      this.loopAudioMeta.stop();
+      clearTimeout(this.exitTimeout);
+    }, startIn)
   }
 }
