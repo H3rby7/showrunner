a7749c5cd15bb22b95aff1911e11d41edd4ec088
h3rby7 <>
2022-02-11 17:02:16 +0100

    introduce custom HTML element 'sound-effect' that will create and bind the audios as previously

--- js/generic.js
+++ js/generic.js
@@ -1,126 +1,85 @@
-class AudioMeta {
-  id;
-  isPlaying = false;
-  button;
-  audio;
-  options;
-  constructor(id) {
-    this.id = id;
-    this.init();
-  }
-
-  play() {
-    console.log("Playing '" + this.id + "'");
-    this.audio.play();
-  }
-
-  stop() {
-    console.log("Stopping '" + this.id + "'");
-    this.audio.pause();
-    this.audio.currentTime = 0;
-  }
-
-  getMsLeft() {
-    return 1000 * (this.audio.duration - this.audio.currentTime);
-  }
-
-  init() {
-    console.log("Binding '" + this.id + "'");
-
-    // DOM elements
-    const container = document.getElementById(this.id);
-    this.button = container.querySelector("button");
-    this.audio = container.querySelector("audio");
-
-    // STATE TRANSITIONS by Events
-    this.audio.addEventListener("ended", () => {
-      this.isPlaying = false;
-      this.button.classList.remove("playing");
-    });
-    this.audio.addEventListener("pause", () => {
-      this.isPlaying = false;
-      this.button.classList.remove("playing");
-    });
-    this.audio.addEventListener("play", () => {
-      this.isPlaying = true;
-      this.button.classList.add("playing");
-    });
-  }
+function DOMrenderSoundEffect() {
+  document.querySelectorAll("sound-effect").forEach(el => {
+    const content = createSoundEffectNode(el);
+    el.innerHTML = "";
+    el.appendChild(content);
+  });
 }
 
-class StoppableAudio extends AudioMeta {
-  init() {
-    super.init();
-    this.button.addEventListener("click", () => {
-      if (!this.isPlaying) {
-        return this.play();
+function createSoundEffectNode(root) {
+  root.playing = false;
+
+  const src = root.attributes.src.value;
+  const type = root.attributes.type.value;
+  const resetting = root.attributes.reset;
+  const loop = root.attributes.loop;
+
+  const div = document.createElement("div");
+  div.classList.add("audio-group");
+
+  const btn = document.createElement("button");
+  btn.innerHTML = root.innerHTML;
+
+  const audio = DOMcreateAudio(src, type, loop);
+
+  div.appendChild(btn);
+  div.appendChild(audio);
+
+  linkControls(root, btn, audio);
+  if (resetting) {
+    makeResetable(btn, audio);
+  } else {
+    makeStoppable(btn, audio);
+  }
+
+  return div;
+
+  function makeResetable() {
+    btn.classList.add("reset");
+    btn.addEventListener("click", () => {
+      audio.currentTime = 0;
+      audio.play();
+    });
+  }
+  
+  function makeStoppable() {
+    btn.addEventListener("click", () => {
+      if (root.playing) {
+        audio.pause();
       } else {
-        return this.stop();
+        audio.currentTime = 0;
+        audio.play();
       }
     });
   }
 }
 
-class SoundEffect extends AudioMeta {
-  init() {
-    super.init();
-    this.button.classList.add("reset");
-    this.button.addEventListener("click", () => {
-      this.audio.currentTime = 0;
-      return this.play();
-    });
-  }
+function DOMcreateAudio(src, type, loop) {
+  const audio = document.createElement("audio");
+  audio.src = src;
+  audio.type = type;
+  audio.preload = "audo";
+  audio.controls = true;
+  if (loop) audio.loop = true;
+  return audio;
 }
 
-// Latency between telling to use the 'loop-out' and the predicted start of the soundfile.
-// Higher Value means earlier Start of 'loop-out'
-const playLatencyMs = 250;
-
-class QuitableLoop {
-
-  loopAudioMeta;
-  endAudioMeta;
-
-  exitTimeout;
-
-  constructor(loopSelector, endSelector) {
-    this.loopAudioMeta = new AudioMeta(loopSelector);
-    this.endAudioMeta = new AudioMeta(endSelector);
-    this.init();
-  }
-
-  init() {
-    this.loopAudioMeta.button.addEventListener("click", () => {
-      this.startLoop();
-    });
-    this.endAudioMeta.button.addEventListener("click", () => {
-      this.exitLoopAndEnd();
-    });
-  }
-
-  startLoop() {
-    this.endAudioMeta.stop();
-    this.loopAudioMeta.audio.loop = true;
-    this.loopAudioMeta.play();
-  }
-
-  stop() {
-    this.endAudioMeta.stop();
-    this.loopAudioMeta.stop();
-  }
-
-  exitLoopAndEnd() {
-    this.loopAudioMeta.audio.loop = false;
-    this.endAudioMeta.audio.currentTime = 0;
-    if (this.exitTimeout) {
-      clearTimeout(this.exitTimeout);
-    }
-    const startIn = this.loopAudioMeta.getMsLeft() - playLatencyMs;
-    this.exitTimeout = setTimeout(() => {
-      this.endAudioMeta.audio.currentTime = 0;
-      this.endAudioMeta.play();
-      this.loopAudioMeta.stop();
-      clearTimeout(this.exitTimeout);
-    }, startIn)
-  }
+function linkControls(parent, btn, audio) {
+  // STATE TRANSITIONS by Events
+  audio.addEventListener("ended", () => {
+    parent.playing = false;
+    btn.classList.remove("playing");
+  });
+  audio.addEventListener("pause", () => {
+    parent.playing = false;
+    btn.classList.remove("playing");
+  });
+  audio.addEventListener("play", () => {
+    parent.playing = true;
+    btn.classList.add("playing");
+  });
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  DOMrenderSoundEffect();
+});
